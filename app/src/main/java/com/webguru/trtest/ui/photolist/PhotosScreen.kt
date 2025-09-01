0010dbe898a45a4b215955d810df12fb74771261
Yatin Sharma <yatin.sharma1991@gmail.com>
2025-09-01 17:02:47 -0600

    added photo list screen

--- app/src/main/java/com/webguru/trtest/ui/photolist/PhotosScreen.kt
+++ app/src/main/java/com/webguru/trtest/ui/photolist/PhotosScreen.kt
@@ -1,18 +1,30 @@
 package com.webguru.trtest.ui.photolist
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import coil.imageLoader
 import com.webguru.trtest.data.model.Photo
 
 @Composable
@@ -23,17 +35,30 @@ fun PhotosMainScreen(
 ) {
     val photoUiState by viewModel.photoUiState.collectAsStateWithLifecycle()
     when (photoUiState) {
-        is PhotoUiState.Loading -> Text("Loading")
+        is PhotoUiState.Loading -> {
+            Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                CircularProgressIndicator()
+            }
+        }
         is PhotoUiState.Error -> Text("Error: ${(photoUiState as PhotoUiState.Error).errorMessage}")
-        is PhotoUiState.Success ->  PhotosMainScreen(modifier, (photoUiState as PhotoUiState.Success).photos)
+        is PhotoUiState.Success -> PhotosMainScreen(
+            modifier,
+            (photoUiState as PhotoUiState.Success).photos
+        )
     }
 }
 
 @Composable
 fun PhotosMainScreen(modifier: Modifier = Modifier, photos: List<Photo>) {
-    Column(modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
         Text("Photos")
-        LazyColumn {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 180.dp),
+            contentPadding = PaddingValues(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier.fillMaxSize()
+        ) {
             items(items = photos, key = { it.id }) { photo ->
                 PhotoItem(photo)
             }
@@ -42,10 +67,28 @@ fun PhotosMainScreen(modifier: Modifier = Modifier, photos: List<Photo>) {
 }
 
 @Composable
-fun PhotoItem(photo: Photo){
-//    val imageLoader = LocalContext.current.imageLoader
-    AsyncImage(
-        model = photo.smallUrl,
-        contentDescription = photo.id
-    )
+fun PhotoItem(photo: Photo) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 5.dp),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardColors(
+            contentColor = MaterialTheme.colorScheme.onSurface,
+            containerColor = MaterialTheme.colorScheme.surface,
+            disabledContentColor = CardDefaults.cardColors().disabledContentColor,
+            disabledContainerColor = CardDefaults.cardColors().disabledContainerColor
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+            AsyncImage(
+                model = photo.smallUrl,
+                contentDescription = photo.id
+            )
+        }
+    }
 }
